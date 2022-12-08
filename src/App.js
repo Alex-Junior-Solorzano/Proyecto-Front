7f7d5a07e9752b13e9f0dd7cba5ac4ec9c6f37ad
Alex-Junior-Solorzano <alex-solorzano07@hotmail.com>
2022-12-07 20:58:03 -0500

    login y registro son responsive pero no funcionales

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Routes, Route, HashRouter } from 'react-router-dom'
+
+import Login from "./components/Login";
+import Registro from "./components/Registro";
+import HomePage from './pages/HomePage';
+import './styles/styles.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <HashRouter >
+        <Routes >
+          <Route path='/' element = {<Login />} />
+          <Route path='/login' element = {<Login />} />
+          <Route path='/registro' element = {<Registro />} />
+          <Route path='/home' element = {<HomePage />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
